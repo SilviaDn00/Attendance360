0de9686a96c6b6b23926fca1a30bdadadc953219
SilviaDn00 <sdenicolo@codearchitects.com>
2025-03-24 18:02:07 +0100

    feat: attendance-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 
+
 export const routes: Routes = [
+   
    {path:'', redirectTo: '/login', pathMatch: 'full'},
-   {path: 'login', loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)}
+   {path: 'login', loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)},
+
+   {path:'', loadChildren:()=> import('./attendance-routes').then(c=>c.attendanceRoutes)}
 ];
---
+++ src/app/attendance-index/attendance-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-attendance-index',
+  imports: [DashboardComponent],
+  templateUrl: './attendance-index.component.html',
+  styleUrl: './attendance-index.component.scss'
+})
+export class AttendanceIndexComponent {
+
+}
