dfcca9ca8aa6d2f08798becd34d09a63505235a2
Davide Colucci <dcolucci@codearchitects.com>
2025-03-25 11:55:54 +0100

    feat: updated login.component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { User } from '../models/users';
+import { UsersService } from '../services/users.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private _userServices = inject(UsersService);
+  public formBuild = inject(FormBuilder);
+  private _router : Router = inject(Router);
+
+  private _users: User[] = [];
+  loginFormGroup: FormGroup;
+
+  constructor() {
+    this.loginFormGroup = this.formBuild.group({
+      username: [''],
+      password: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    this._userServices.getUsers().subscribe((data) => {
+      this._users = data;
+    });
+  }
+
+  login() {
+    if (this.loginFormGroup.valid) { 
+      const user = this._users.find((user) => (user.username === this.loginFormGroup.value.username) && (user.password === this.loginFormGroup.value.password) );
+      if (user) {
+        this._router.navigate(["/dashboard"]);
+      } else {
+        alert('User not found');
+      }
+      
+    }
+  }
 
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+
+  private readonly _http = inject(HttpClient);
+
+  private readonly _url  = 'users.json';
+
+  constructor() { }
+
+  getUsers() : Observable<User[]> {
+    return this._http.get<User[]>(this._url);
+  }
+
+}
