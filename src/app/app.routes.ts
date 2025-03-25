c1a87c767370f9ba9ec44f16514e045f7eb51908
Michele Sette <msette@codearchitects.com>
2025-03-25 12:12:15 +0100

    fix commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { activateAuthGuard } from './guards/activate-auth.guard';
 
 
 
@@ -7,5 +8,6 @@ export const routes: Routes = [
    {path:'', redirectTo: '/login', pathMatch: 'full'},
    {path: 'login', loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)},
 
-   {path:'', loadChildren:()=> import('./attendance-routes').then(c=>c.attendanceRoutes)}
+   {path:'', loadChildren:()=> import('./attendance-routes').then(c=>c.attendanceRoutes), canActivate:[activateAuthGuard],
+      canDeactivate:[]}
 ];
--- src/app/attendance-index/attendance-index.component.ts
+++ src/app/attendance-index/attendance-index.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-attendance-index',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, NavbarComponent],
   templateUrl: './attendance-index.component.html',
   styleUrl: './attendance-index.component.scss'
 })
