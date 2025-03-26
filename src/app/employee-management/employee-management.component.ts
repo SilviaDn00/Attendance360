e081b8e96dbd7ae7ae64fbd3f7bcf937aaa437d6
Michele Sette <msette@codearchitects.com>
2025-03-26 14:53:33 +0100

    feat: added form and table on employee-management.component.html

--- src/app/employee-management/employee-management.component.ts
+++ src/app/employee-management/employee-management.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-employee-management',
-  imports: [],
+  imports: [MatRadioModule,MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './employee-management.component.html',
-  styleUrl: './employee-management.component.scss'
+  styleUrl: './employee-management.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeManagementComponent {
 
 }
+
