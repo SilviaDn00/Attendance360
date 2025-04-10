b4e64ee6f977d77a4fd4201fb3edb989c44a843f
SilviaDn00 <sdenicolo@codearchitects.com>
2025-04-10 12:39:50 +0200

    feat: implement dashboard with dynamic card components and ng-content

--- src/app/admin-area/dashboard/dashboard.component.ts
+++ src/app/admin-area/dashboard/dashboard.component.ts
@@ -1,14 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { LoginService } from '../../login-area/services/login.service';
+import { CardComponent } from '../../card/card.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
   public logService = inject(LoginService);
+
+  protected readonly Items = [
+    { title: 'Numero totale dei dipendenti:', text: this.getTotalEmployees(), action: 'pulsante' },
+    { title: 'Timbrature di oggi:', text: this.getTodayStampsCount(), action: 'pulsante' },
+    { title: 'Percentuale presenti:', text: this.getTodayPresencePercentage(), action: 'pulsante' },
+    { title: 'Alert sulle anomalie', text: this.getAnomalyAlerts(), action: 'pulsante' },
+  ]
+
+  getTotalEmployees() {
+    console.log('ciao')
+
+  }
+
+  getTodayStampsCount() {
+    console.log('migheleeeeee')
+  }
+
+  getTodayPresencePercentage() {
+    console.log('migheleeeeee')
+  }
+
+  getAnomalyAlerts() {
+    console.log('migheleeeeee')
+  }
+
   
+
 }
