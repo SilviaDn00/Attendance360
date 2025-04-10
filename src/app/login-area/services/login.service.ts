39d05db9ad767c38f3642f077277741c674fb96a
Davide Colucci <dcolucci@codearchitects.com>
2025-04-10 10:02:29 +0200

    refactor: update User model to include name and surname, modify login service to store and retrieve full name

--- src/app/login-area/services/login.service.ts
+++ src/app/login-area/services/login.service.ts
@@ -24,7 +24,8 @@ export class LoginService {
     // Se esiste un utente che corrisponde, restituiamo true (accesso consentito)
     if (user) {
       localStorage.setItem('id', user.id);
-      localStorage.setItem('username', user.username)
+      localStorage.setItem('name', user.name);
+      localStorage.setItem('surname', user.surname);
       localStorage.setItem('role', user.role);
       return true;
     } else {
@@ -38,7 +39,10 @@ export class LoginService {
   }
 
   getUsername(): string | null {
-    return localStorage.getItem('username')
+    let n = this._users.find(u => u.id === localStorage.getItem('id'))?.name || null;
+    let s = this._users.find(u => u.id === localStorage.getItem('id'))?.surname || null;
+    let username = n + ' ' + s;
+    return username;
   }
 
   logout(): void{
--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -1,3 +1,3 @@
 export class User {      
-    constructor( public id: string, public username: string, public email: string, public password: string, public role: string, public department : string ) {}
+    constructor( public id: string, public name: string, public surname: string, public email: string, public password: string, public role: string, public department : string ) {}
 }
