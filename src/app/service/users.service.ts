5c9904f5bf4f49d8641dd4bf52d007a65c4e8536
Davide Colucci <dcolucci@codearchitects.com>
2025-04-16 17:36:51 +0200

    feat: enhance user form and employee details components; implement edit functionality and update routing

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -12,7 +12,7 @@ export class UsersService {
   private readonly _urlUsers = 'http://localhost:5077/api/User/GetUsers'; // URL del tuo endpoint API
   private readonly _urlUser = 'http://localhost:5077/api/User/GetUser'; // URL del tuo endpoint API
   private readonly _urlPost = 'http://localhost:5077/api/User/PostUser'; // URL del tuo endpoint API
-  private readonly _urlUpdate = 'http://localhost:5077/api/User/UpdateUser'; // URL del tuo endpoint API
+  private readonly _urlUpdate = `http://localhost:5077/api/User/UpdateUser`; // URL del tuo endpoint API
   private readonly _urlEnabled = 'http://localhost:5077/api/User/EnabledUser'; // URL del tuo endpoint API
 
 
@@ -29,8 +29,11 @@ export class UsersService {
     return this._http.post<User[]>(this._urlPost, user);
   }
 
-  UpdateUsers(user: User): Observable<User[]> {
-    return this._http.put<User[]>(this._urlUpdate, user);
+  UpdateUsers( user : User): Observable<User[]> {
+    console.log('user', user);   
+    console.log();
+    
+    return this._http.put<User[]>(`${this._urlUpdate}/${user.id}`, user);
   }
 
   UpdateUserEnabled(id: string): Observable<User> {
